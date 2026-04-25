719b723d60d94b0d7562b97f0d9bce27747de605
JulianDavidRamirezManrique <jramirezman@unal.edu.co>
2026-04-25 18:06:48 -0500

    favicon y descripcion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="Professional mobile app development, Votaciones en tiempo real, quórum dinámico, reportes auditables con hash SHA-256." />
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Asambli') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
